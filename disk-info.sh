00f5806659b8ff782c4738bc69df301b4c869709
brando56894 <brando56894@gmail.com>
2019-07-05 10:43:04 -0400

    fixes

--- disk-info.sh
+++ disk-info.sh
@@ -4,9 +4,13 @@ for i in `ls /dev/disk/by-id|grep wwn|grep -v part`
   do
    echo "device $i is"
    sudo smartctl -i /dev/disk/by-id/$i|head -n7|tail -n3
-   echo "Capacity: $(sudo smartctl -a /dev/disk/by-id/$i|grep bytes|head -n1|awk '{print $5, $6}'|cut -c 2-8)"
+   echo "Capacity: $(sudo smartctl -a /dev/disk/by-id/$i|grep bytes|head -n1|awk '{print $5, $6}'|cut -c 2-8|tr -d ']')"
    echo "Health: $(sudo smartctl -H /dev/disk/by-id/$i|head -n5|tail -n1|awk '{print $6}')"
-   echo "Temperature (C): $(sudo smartctl -a /dev/disk/by-id/$i|grep -e 194 -e 190|awk '{print $10}')"
+   if [[ $i == "wwn-0x50025385a027fbce" ]]; then
+   	echo "Temperature (C): $(sudo smartctl -a /dev/disk/by-id/$i|grep 190|awk '{print $10}')"
+   else
+   	echo "Temperature (C): $(sudo smartctl -a /dev/disk/by-id/$i|grep 194|awk '{print $10}')"
+   fi
    echo ""
 done
 
