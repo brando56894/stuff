f650cd687d948d61fd50ddf7b638d30766449504
Brandon <brando56894@users.noreply.github.com>
2023-01-11 16:53:12 -0500

    Update pcielanes.sh
    comment

--- pcielanes.sh
+++ pcielanes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 totalwidth=0
+#removes any lines mentioning the CPU, if it's intel replace it but it will also ignore NICs made by intel
 for i in $(lspci|grep -viE 'amd'|awk '{print $1}')
   do
 	  width=$(sudo lspci -vvs $i 2>/dev/null|grep -i lnksta|grep -vi lnksta2|awk '{print $5}'|tr -d 'x' ) 
