761005aecdd8b8f496f0b505754bd21ebe742ebb
Brandon Golway <brandon56894@gmail.com>
2023-11-20 23:51:29 -0500

    removing  useless echos

--- mount-buckets.sh
+++ mount-buckets.sh
@@ -29,10 +29,8 @@ for i in $BUCKETS;do
   OPTIONS="--daemon --verbose --verbose --no-modtime --vfs-cache-mode full --transfers 8 --allow-other --log-file=/var/log/rclone/$i.log  --vfs-cache-max-size=180G  --vfs-cache-max-age=5m"
   if [[ $i == "requested-shows" ]]; then
     rclone mount $OPTIONS idrive:$i /mnt/idrive/requests
-    echo
   else
     rclone mount $OPTIONS idrive:$i /mnt/idrive/$i
-    echo
   fi
 done
 
